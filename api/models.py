c267ef47a262d6588a371fd0cd240fa93a902e3d
KavyaV00 <44108726+KavyaV00@users.noreply.github.com>
2021-11-23 21:03:54 +0530

    Updated models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Victim(models.Model):
+    location = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    latitude = models.DecimalField(max_digits=19, decimal_places=16)
+    longitude = models.DecimalField(max_digits=19, decimal_places=16)
+    date_created = models.DateTimeField(auto_now_add=True)
+    additional_info = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.location + ' - ' + self.additional_info
+
+class Volunteer(models.Model):
+    name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=15)
+    age = models.IntegerField()
+    qualification = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + ' - ' + self.qualification
+
+class Camp(models.Model):
+    location = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    latitude = models.DecimalField(max_digits=19, decimal_places=16)
+    longitude = models.DecimalField(max_digits=19, decimal_places=16)
+    max_capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.location + ' - ' + self.max_capacity
+    
+
+    
