cb05419344ae0690325a749a22eb1d651001c95e
Sandeep Pillai <sandeep.pillai42@gmail.com>
2021-11-24 16:21:08 +0530

    Update settings.py

--- asap/settings.py
+++ asap/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
