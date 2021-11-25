dfd450842940f6bb24899d6d58bd10e6d6539f48
Sandeep Pillai <sandeep.pillai42@gmail.com>
2021-11-25 11:16:21 +0530

    heroku db test 3

--- asap/settings.py
+++ asap/settings.py
@@ -153,5 +153,5 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
-
+import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
